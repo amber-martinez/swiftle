755e9662e87626233f2eb15adcd06cb525f2bf79
Amber Martinez <martinez.amber1028@gmail.com>
2023-03-18 15:37:19 -0700

    Position letters inside of boxes in matrix

--- client/src/Components/Matrix/Row.js
+++ client/src/Components/Matrix/Row.js
@@ -2,26 +2,23 @@ import React from 'react';
 
 function Row({ row }) {
     return (
-        <div style={{ textAlign: 'center', margin: -3, fontFamily: "Sono", fontSize: 24, color: "#69696b" }}>
+        <div id="row">
             <div id='box'>
-                <p style={{ margin: 7 }}>{row[0]}</p>
+                <p id="letter">{row[0]}</p>
             </div>
             <div id='box'>
-                <p style={{ margin: 7 }}>{row[1]}</p>
+                <p id="letter">{row[1]}</p>
             </div>
             <div id='box'>
-                <p style={{ margin: 7 }}>{row[2]}</p>
+                <p id="letter">{row[2]}</p>
             </div>
             <div id='box'>
-                <p style={{ margin: 7 }}>{row[3]}</p>
+                <p id="letter">{row[3]}</p>
             </div>
             <div id='box'>
-                <p style={{ margin: 7 }}>{row[4]}</p>
+                <p id="letter">{row[4]}</p>
             </div>
         </div>
-        // <div style={{ fontFamily: "Sono", letterSpacing: 2, color: '#7f7e7c' }}>
-        //     taylor
-        // </div>
     )
 }
 
