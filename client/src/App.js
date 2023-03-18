0ef4e042f54bf36e2a889425182b78711813f797
Amber Martinez <martinez.amber1028@gmail.com>
2023-03-18 14:37:05 -0700

    Add string state

--- client/src/App.js
+++ client/src/App.js
@@ -7,12 +7,13 @@ function App() {
   // let's do a matrix filled with stacks. can only remove from top
   const [currCol, setCurrCol] = useState(0);
   const [currRow, setCurrRow] = useState(0);
+  const [currStr, setCurrStr] = useState("");
 
   return (
     <div className="App">
       <Header/>
       <RowGroup currCol={currCol} currRow={currRow}/>
-      <Keyboard setCurrCol={setCurrCol} setCurrRow={setCurrRow} currCol={currCol} currRow={currRow}/>
+      <Keyboard setCurrCol={setCurrCol} setCurrRow={setCurrRow} currCol={currCol} currRow={currRow} currStr={currStr} setCurrStr={setCurrStr}/>
     </div>
   );
 }
--- client/src/Components/Keyboard/Key.js
+++ client/src/Components/Keyboard/Key.js
@@ -1,18 +1,21 @@
 import React from 'react';
 
-function Key({ letter, setCurrCol, setCurrRow, currCol, currRow }) {
+function Key({ letter, setCurrCol, setCurrRow, setCurrStr, currCol, currRow, currStr }) {
     
-    function progressMatrix() {
+    function handleNewLetter(e) {
+        let letter = e.target.value
         if (currCol == 5) {
             setCurrCol(1)
             setCurrRow(currRow+1)
+            setCurrStr(letter)
         } else {
             setCurrCol(currCol+1)
+            setCurrStr(currStr+=letter)
         }
     }
 
     return (
-        <button id='key' onClick={progressMatrix}>
+        <button id='key' value={letter} onClick={handleNewLetter}>
             {letter}
         </button>
     )
